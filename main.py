ec4bdf1a84faeffe23aee0ece62f2be6cc2d6ac1
Your Name <610mamtabhatt@gmail.com>
2025-12-04 13:01:31 -0600

    added game image

--- main.py
+++ main.py
@@ -11,14 +11,25 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
+#Player
+playerImg = pygame.image.load('player.png',)
+small_icon_player = pygame.transform.scale(playerImg, (64,64))
+
+playerX = 350
+playerY = 480
+
+def player():
+    screen.blit(small_icon_player, (playerX, playerY))
+
 #Game loop
 running  = True
 while running:
+    # RGB- Red, Green, Blue
+    screen.fill((64, 224, 208))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running =False
 
-
-    #RGB- Red, Green, Blue
-    screen.fill((64,224,208))
+    player()
     pygame.display.update()
