d0ee90fc508c7f0357da432fae8c63245fc443cc
Your Name <610mamtabhatt@gmail.com>
2025-12-09 21:56:21 -0600

    keyboard input controls

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ small_icon_player = pygame.transform.scale(playerImg, (64,64))
 
 playerX = 350
 playerY = 480
+playerX_change = 0
 
 def player(x, y):
     screen.blit(small_icon_player, (x, y))
@@ -26,11 +27,28 @@ running  = True
 while running:
     # RGB- Red, Green, Blue
     screen.fill((64, 224, 208))
-    playerY -= 0.1
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running =False
 
+        #if keystroke is pressed check whether its right or left
+        if event.type == pygame.KEYDOWN:
+            print("A Keystoke has been pressed")
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+
+    # 5 = 5 + -0.1 -> 5 = 5 - 0.1
+    # 5 = 5 + 0.1
+
+    playerX += playerX_change
     player(playerX, playerY)
     pygame.display.update()
